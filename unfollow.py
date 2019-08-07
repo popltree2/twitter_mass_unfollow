fb5f1edaaeb5fc751a88e4f35d62af7bb506a6ac
Cody Clements <cody.clements@gmail.com>
2019-08-06 23:06:51 -0700

    Update unfollow.py
    Added friend count before unfollow, confirmation, and total number of friends unfollowed. destroy_friendship commented out during testing.

--- unfollow.py
+++ unfollow.py
@@ -12,17 +12,30 @@ ACCESS_TOKEN_SECRET = keys['access_token_secret']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 start = time.time()
 i = 0
 friends=api.friends_ids(SCREEN_NAME)
 
-while (1):
-    try:
+print("You have", len(friends), "friends.")
+while(1):
+    proceed=input("Proceeding will unfollow all accounts. Are you sure you'd like to proceed [Y/N]: ")
+    if proceed.lower() == "y" or proceed.lower() == "yes":
         for u in friends:
-            print ("Unfollowing", api.get_user(u).screen_name)
-            api.destroy_friendship(u)
-    except:
-        print "Possibly rate limited. Waiting 60 seconds."
-        time.sleep(60)
+            print("Unfollowing", api.get_user(u).screen_name)
+#           api.destroy_friendship(u)
+            i = i + 1
+        if i == 1:
+            print(i, "friend unfollowed")
+            print ()
+        else:
+            print(i, "friends unfollowed")
+            print()
+        break
+
+    elif proceed.lower() == "n" or proceed.lower() == "no":
+        print("Stopping Unfollow")
+        break
+    else:
+        print("Invalid Selection. Try again.")
